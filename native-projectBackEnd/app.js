576d41f42c406e7fb685d5000b577a5fde1aefe1
Damon Chivers <chiversdamon@gmail.com>
2018-10-31 12:16:15 -0600

    post and get routes created

--- native-projectBackEnd/app.js
+++ native-projectBackEnd/app.js
@@ -19,15 +19,53 @@ app.use(function (err, req, res, next) {
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true
 });
+const menuSchema = new mongoose.Schema({
+    item: String,
+    price: Number
+})
 
-app.get('/ping', (req,res) => {
-    res.send('PONG!')
+const restaurantsSchema = new mongoose.Schema({
+    name: String,
+    level: Number,
+    menuItems: [menuSchema]
+})
+
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    seatNumber: Number,
+    order: [menuSchema]
+})
+
+const Restaurants = mongoose.model('restaurants', restaurantsSchema)
+const Users = mongoose.model('users', userSchema)
+
+app.get('/restaurants', (req,res) => {
+    Restaurants.find({}, function(err, result){
+        console.log(err, result)
+    }).then(restaurant => res.status(201).json({restaurant}))
   })
+
+
+app.get('/users', (req,res) => {
+    Users.find({}, function(err, result){
+        console.log(err, result)
+    }).then(users => res.status(201).json({users}))
+  })
+
+app.post('/users', (req, res) =>{
+    Users.create(req.body).then(user => res.status(201).json({user}))
+})
+
+app.post('/restaurants', (req, res) =>{
+    Restaurants.create(req.body).then(restaurant => res.status(201).json({restaurant}))
+})
   
-  app.use((err,req,res,next)=>{
+app.use((err,req,res,next)=>{
     res.status(err.status || 500).json({error:err})
   })
-  app.use((req,res,next)=>{
+
+app.use((req,res,next)=>{
     res.status(404).json({error: {message: 'Not Found!'}})
   })
 
