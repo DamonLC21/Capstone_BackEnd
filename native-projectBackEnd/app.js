76e58fb63ff8f152f575eea698f5f52e549904a5
Damon Chivers <chiversdamon@gmail.com>
2018-11-06 14:40:24 -0700

    update to sessionSchema to add name as well

--- native-projectBackEnd/app.js
+++ native-projectBackEnd/app.js
@@ -36,6 +36,7 @@ const locationSchema = new mongoose.Schema({
 })
 
 const sessionsSchema = new mongoose.Schema({
+    sessionName: String,
     location1: locationSchema,
     location2: locationSchema,
     
@@ -86,39 +87,3 @@ app.use((req,res,next)=>{
 app.listen(port , () => {
     console.log(`listening on ${port} yaherrrd`)
 })
-
-
-const placeObject = (yourlocation, mylocation, isMiles,) =>{ 
-      
-    function toRad(x) {
-        return x * Math.PI / 180;
-      }
-     
-      var lon1 = yourlocation.lng;
-      var lat1 = yourlocation.lat;
-     
-      var lon2 = mylocation.lng;
-      var lat2 = mylocation.lat;
-     
-      var R = 6371; // km
-     
-      var x1 = lat2 - lat1;
-      var dLat = toRad(x1);
-      var x2 = lon2 - lon1;
-      var dLon = toRad(x2)
-      var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-      var d = R * c;
-    
-     
-      if(isMiles) d /= 1.60934;
-      x1 = x1 * 1609.344
-      x2 = x2 * 1609.344 
-      d = d * 1609.344
-     
-      return [x1, x2, d]
-  }
-
-  console.log(placeObject({lat:39.75751913899073,lng:-105.00692868825644},{lat:39.75747517814582755,lng:-105.00690361273335},true))
