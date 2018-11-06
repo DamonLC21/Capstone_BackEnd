bd7d39f22677870610eefa9ae394c32b42cb4bd8
Damon Chivers <chiversdamon@gmail.com>
2018-11-05 17:35:11 -0700

    schema updated

--- native-projectBackEnd/app.js
+++ native-projectBackEnd/app.js
@@ -69,7 +69,8 @@ app.post('/users', (req, res) =>{
 })
 
 app.post('/sessions', (req, res) =>{
-    Sessions.create(req.body).then(session => res.status(201).json({session}))
+    Sessions.create(req.body,{},function (err, result) { 
+        console.log(err,result)}).then(session => res.status(201).json({session}))
 })
   
 app.use((err,req,res,next)=>{
